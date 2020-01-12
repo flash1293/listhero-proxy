333823d69de02e33452dde3e51543484171df64f
Johannes Reuter <email@johannes-reuter.de>
2020-01-12 20:29:16 +0100

    proxy instead of redirect and add websocket support

--- src/index.js
+++ src/index.js
@@ -1,42 +1,75 @@
+const proxy = require("express-http-proxy");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const WebSocket = require('ws');
 const info = require("debug")("info");
+const error = require("debug")("error");
 
-  const proxyUrl = process.env.PROXY_URL || 'http://localhost:3001';
+const proxyUrl = process.env.PROXY_URL || "http://localhost:3001";
+const proxyWsUrl = proxyUrl.replace("http", "ws");
 
-  const app = express();
-  const expressWs = require("express-ws")(app);
-  app.use(bodyParser.json({ type: "application/json", limit: "2mb" }));
-  app.use(cors());
-  app.post(
-    "/token",
-    (req, res) => {
-      info("Forwarding token request");
-      res.redirect(`${proxyUrl}/token`);
-    }
-  );
+const app = express();
+const expressWs = require("express-ws")(app);
+app.use(bodyParser.json({ type: "application/json", limit: "2mb" }));
+app.use(cors());
+app.post("/token", function(req, res, next) {
+  info(`${new Date()} Proxying token request`);
+  next();
+});
+app.post("/api", function(req, res, next) {
+  info(`${new Date()} Proxying api request`);
+  next();
+});
+app.post(
+  "/token",
+  proxy(proxyUrl, {
+    proxyReqPathResolver: () => "/token"
+  })
+);
+app.post(
+  "/api",
+  proxy(proxyUrl, {
+    proxyReqPathResolver: () => "/api"
+  })
+);
 
-  app.post(
-    "/api",
-    async (req, res) => {
-      info("Forwarding api request");
-      res.redirect(`${proxyUrl}/api`);
-    }
-  );
+app.ws("/api/updates/:session", (ws, req) => {
+  info(`${new Date()} Proxying web socket update connection`);
+  const wsUrl = `${proxyWsUrl}/api/updates/${req.params.session}`;
+  const proxyWs = new WebSocket(wsUrl, req.headers["sec-websocket-protocol"]);
 
-  app.get("/api/updates/:session", (ws, req) => {
-    info("Forwarding websocket session request");
-    res.redirect(`${proxyUrl}${req.path}`);
+  proxyWs.on("open", function() {
+    info("Forwarded websocket connection established");
   });
 
-  const port = process.env.PORT || 3002;
-  const server = app.listen(port, () =>
-    info(`Ekofe proxy server listening on port ${port}! Forwarding all requests to ${proxyUrl}`)
-  );
-
-  process.on("SIGTERM", function() {
-    server.close(function() {
-      process.exit(0);
-    });
+  proxyWs.on("error", function(e) {
+    error(e);
+    ws.close();
   });
+
+  proxyWs.on("message", function(data) {
+    ws.send(data);
+  });
+
+  proxyWs.on("close", function() {
+    ws.close();
+  });
+
+  ws.on("close", function() {
+    proxyWs.close();
+  });
+});
+
+const port = process.env.PORT || 3002;
+const server = app.listen(port, () =>
+  info(
+    `Ekofe proxy server listening on port ${port}! Forwarding all requests to ${proxyUrl}`
+  )
+);
+
+process.on("SIGTERM", function() {
+  server.close(function() {
+    process.exit(0);
+  });
+});
